6da5fc7321088782752f5f66d18e0f5ec5f550b7
Guilherme Canuto <ganuto@hotmail.com>
2024-08-28 20:57:49 -0300

    Added initial structure; Added controllers; Added new request/response DTO for the account controller

---
+++ src/main/java/com/project/transactions/controller/TransactionController.java
@@ -0,0 +1,4 @@
+package com.project.transactions.controller;
+
+public class TransactionController {
+}
---
+++ src/main/java/com/project/transactions/controller/data/request/AccountCreationRequest.java
@@ -0,0 +1,14 @@
+package com.project.transactions.controller.data.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+public class AccountCreationRequest {
+    @NotNull(message = "'documentNumber' cannot be null")
+    private Long documentNumber;
+}
