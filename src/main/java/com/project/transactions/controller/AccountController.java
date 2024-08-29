fddeaa1f86b2dc478781cf65e1eb914cfbaf1535
Guilherme Canuto <ganuto@hotmail.com>
2024-08-29 17:04:24 -0300

    Added new endpoints to AccountController; Added new dtos

--- src/main/java/com/project/transactions/controller/AccountController.java
+++ src/main/java/com/project/transactions/controller/AccountController.java
@@ -2,20 +2,25 @@ package com.project.transactions.controller;
 
 import com.project.transactions.controller.data.request.AccountCreationRequest;
 import com.project.transactions.controller.data.response.AccountCreationResponse;
+import com.project.transactions.controller.data.response.AccountResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/accounts")
 public class AccountController {
 
+    @PostMapping
     public ResponseEntity<AccountCreationResponse> create(@RequestBody @Valid AccountCreationRequest accountCreationRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(new AccountCreationResponse());
+    }
+
+    @GetMapping
+    public ResponseEntity<AccountResponse> get(@RequestParam Long accountId){
+        return ResponseEntity.ok(new AccountResponse());
     }
 }
---
+++ src/main/java/com/project/transactions/controller/data/response/AccountResponse.java
@@ -0,0 +1,11 @@
+package com.project.transactions.controller.data.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountResponse {
+    private Long documentNumber;
+    private Long accountId;
+}
