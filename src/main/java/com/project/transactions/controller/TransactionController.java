4548bd10007a8f79617bd11f85676858240f7aed
Guilherme Canuto <ganuto@hotmail.com>
2024-08-29 20:19:21 -0300

    TransactionController small adjustments

--- src/main/java/com/project/transactions/controller/TransactionController.java
+++ src/main/java/com/project/transactions/controller/TransactionController.java
@@ -1,8 +1,12 @@
 package com.project.transactions.controller;
 
+import com.project.transactions.controller.data.request.TransactionRequest;
 import com.project.transactions.controller.data.response.TransactionResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/transactions")
 public class TransactionController {
 
-    public ResponseEntity<TransactionResponse> execute(){
+    @PostMapping
+    public ResponseEntity<TransactionResponse> execute(@RequestBody @Valid TransactionRequest transactionRequest) {
         return ResponseEntity.ok(new TransactionResponse());
     }
 }
