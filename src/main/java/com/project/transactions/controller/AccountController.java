10534226b6124d642a19525e2a095bf35e5ea874
Guilherme Canuto <ganuto@hotmail.com>
2024-08-28 21:03:26 -0300

    AccountController shell

--- src/main/java/com/project/transactions/controller/AccountController.java
+++ src/main/java/com/project/transactions/controller/AccountController.java
@@ -1,4 +1,21 @@
 package com.project.transactions.controller;
 
+import com.project.transactions.controller.data.request.AccountCreationRequest;
+import com.project.transactions.controller.data.response.AccountCreationResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/accounts")
 public class AccountController {
+
+    public ResponseEntity<AccountCreationResponse> create(@RequestBody @Valid AccountCreationRequest accountCreationRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(new AccountCreationResponse());
+    }
 }
