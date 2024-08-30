140ab9eb77215c1280886a5aae52a7c648106d31
Guilherme Canuto <ganuto@hotmail.com>
2024-08-30 18:20:19 -0300

    Added account validation in TransactionService flow

--- src/main/java/com/project/transactions/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/project/transactions/service/impl/TransactionServiceImpl.java
@@ -5,6 +5,7 @@ import com.project.transactions.controller.data.response.TransactionResponse;
 import com.project.transactions.domain.Transaction;
 import com.project.transactions.mapper.TransactionMapper;
 import com.project.transactions.repository.TransactionRepository;
+import com.project.transactions.service.AccountService;
 import com.project.transactions.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,17 @@ import org.springframework.stereotype.Service;
 public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final AccountService accountService;
 
     @Override
     public TransactionResponse execute(TransactionRequest transactionRequest) {
+        validateAccountExistence(transactionRequest.getAccountId());
         Transaction transaction = TransactionMapper.toDomain(transactionRequest);
         Transaction persistedTransaction = transactionRepository.save(transaction);
         return TransactionMapper.toResponse(persistedTransaction);
+    }
+
+    private void validateAccountExistence(Long accountID){
+        accountService.findById(accountID);
     }
 }
