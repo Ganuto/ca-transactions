54cbda273ac1502381582c3fbb13857dba47a206
Guilherme Canuto <ganuto@hotmail.com>
2024-08-30 13:17:41 -0300

    Adjusted the findById method in AccountService; Added new constraints to transactionResponse

--- src/main/java/com/project/transactions/controller/data/request/TransactionRequest.java
+++ src/main/java/com/project/transactions/controller/data/request/TransactionRequest.java
@@ -1,5 +1,7 @@
 package com.project.transactions.controller.data.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +10,10 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class TransactionRequest {
+    @NotNull(message = "'documentNumber' cannot be null")
     private Long accountId;
+    @NotNull(message = "'operationTypeId' cannot be null")
     private Integer operationTypeId;
+    @Positive(message = "'amount' cannot be null")
     private BigDecimal amount;
 }
--- src/main/java/com/project/transactions/service/impl/AccountServiceImpl.java
+++ src/main/java/com/project/transactions/service/impl/AccountServiceImpl.java
@@ -3,13 +3,13 @@ package com.project.transactions.service.impl;
 import com.project.transactions.controller.data.request.AccountCreationRequest;
 import com.project.transactions.controller.data.response.AccountResponse;
 import com.project.transactions.domain.Account;
+import com.project.transactions.domain.BusinessException;
 import com.project.transactions.mapper.AccountMapper;
 import com.project.transactions.repository.AccountRepository;
 import com.project.transactions.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -26,9 +26,8 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountResponse findById(Long accountId) {
-        // TODO: Remove null pointer error possibility
         Account account = Optional.of(accountRepository.findById(accountId))
-                .get().orElse(null);
-        return AccountMapper.toResponse(Objects.requireNonNull(account));
+                .get().orElseThrow(() -> new BusinessException(String.format("Account with id: [%s] not found.", accountId)));
+        return AccountMapper.toResponse(account);
     }
 }
