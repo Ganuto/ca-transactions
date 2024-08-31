7de734e7fd596657e29b2d8d2abf27ad39615abf
Guilherme Canuto <ganuto@hotmail.com>
2024-08-30 23:24:51 -0300

    Added operation and amount validation to the transaction flow

--- src/main/java/com/project/transactions/controller/data/request/TransactionRequest.java
+++ src/main/java/com/project/transactions/controller/data/request/TransactionRequest.java
@@ -1,7 +1,6 @@
 package com.project.transactions.controller.data.request;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +13,6 @@ public class TransactionRequest {
     private Long accountId;
     @NotNull(message = "'operationTypeId' cannot be null.")
     private int operationTypeId;
-    @Positive(message = "'amount' cannot be null.")
+    // Adjust to be numbers only
     private BigDecimal amount;
 }
--- src/main/java/com/project/transactions/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/project/transactions/service/impl/TransactionServiceImpl.java
@@ -2,13 +2,17 @@ package com.project.transactions.service.impl;
 
 import com.project.transactions.controller.data.request.TransactionRequest;
 import com.project.transactions.controller.data.response.TransactionResponse;
+import com.project.transactions.domain.OperationType;
 import com.project.transactions.domain.Transaction;
+import com.project.transactions.domain.exception.BusinessException;
 import com.project.transactions.mapper.TransactionMapper;
 import com.project.transactions.repository.TransactionRepository;
 import com.project.transactions.service.AccountService;
 import com.project.transactions.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +23,30 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public TransactionResponse execute(TransactionRequest transactionRequest) {
-        validateAccountExistence(transactionRequest.getAccountId());
         Transaction transaction = TransactionMapper.toDomain(transactionRequest);
+        validateAccountExistence(transaction.getAccountId());
+        validateOperationAndAmount(transaction.getOperationType(), transaction.getAmount());
         Transaction persistedTransaction = transactionRepository.save(transaction);
         return TransactionMapper.toResponse(persistedTransaction);
     }
 
-    private void validateAccountExistence(Long accountID){
+    private void validateAccountExistence(Long accountID) {
         accountService.findById(accountID);
+    }
+
+    private void validateOperationAndAmount(OperationType operationType, BigDecimal amount) {
+        boolean isPositiveAmount = amount.signum() > 0;
+        switch (operationType) {
+            case PAYMENT -> {
+                if (!isPositiveAmount) {
+                    throw new BusinessException(String.format("The operation [%s] cannot have negative amount [%s] .", operationType, amount));
+                }
+            }
+            case INSTALLMENT_PURCHASE, PURCHASE, WITHDRAWAL -> {
+                if (isPositiveAmount) {
+                    throw new BusinessException(String.format("The operation [%s] cannot have positive amount [%s] .", operationType, amount));
+                }
+            }
+        }
     }
 }
