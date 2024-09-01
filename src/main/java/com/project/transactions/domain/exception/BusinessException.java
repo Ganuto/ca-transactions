dfce1233326d47f93b30e825325de9d47382cc69
Guilherme Canuto <ganuto@hotmail.com>
2024-09-01 16:48:28 -0300

    Removed unused method

--- src/main/java/com/project/transactions/domain/exception/BusinessException.java
+++ src/main/java/com/project/transactions/domain/exception/BusinessException.java
@@ -1,7 +1,7 @@
 package com.project.transactions.domain.exception;
 
 public class BusinessException extends RuntimeException {
-    
+
     public BusinessException(String message) {
         super(message);
     }
