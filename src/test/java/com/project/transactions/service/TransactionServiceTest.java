d4f2a2c1583283c66a6e1da42b61b389df681742
Guilherme Canuto <ganuto@hotmail.com>
2024-09-01 13:13:47 -0300

    Added new TransactionService unit tests

--- src/test/java/com/project/transactions/service/TransactionServiceTest.java
+++ src/test/java/com/project/transactions/service/TransactionServiceTest.java
@@ -4,6 +4,7 @@ import com.project.transactions.controller.data.request.TransactionRequest;
 import com.project.transactions.controller.data.response.TransactionResponse;
 import com.project.transactions.domain.OperationType;
 import com.project.transactions.domain.Transaction;
+import com.project.transactions.domain.exception.BusinessException;
 import com.project.transactions.mock.TransactionMock;
 import com.project.transactions.repository.TransactionRepository;
 import com.project.transactions.service.impl.TransactionServiceImpl;
@@ -16,7 +17,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -32,7 +36,7 @@ public class TransactionServiceTest {
     private TransactionServiceImpl transactionService;
 
     @Test
-    public void createTransactionSuccessfully() {
+    public void createPaymentTransactionSuccessfully() {
         TransactionRequest transactionRequest = TransactionMock.createTransactionRequest(OperationType.PAYMENT, BigDecimal.valueOf(10.23));
         Transaction transaction = TransactionMock.createTransaction(OperationType.PAYMENT, BigDecimal.valueOf(10.23));
 
@@ -40,9 +44,23 @@ public class TransactionServiceTest {
 
         TransactionResponse transactionResponse = transactionService.execute(transactionRequest);
 
+        verify(accountService).findById(anyLong());
         assertEquals(transaction.getId(), transactionResponse.getTransactionId());
         assertEquals(transaction.getAccountId(), transactionResponse.getAccountId());
         assertEquals(transaction.getOperationType().getId(), transactionResponse.getOperationTypeId());
         assertEquals(transaction.getAmount(), transactionResponse.getAmount());
     }
+
+    @Test
+    public void createPaymentTransactionUnsuccessfully() {
+        BigDecimal amount = BigDecimal.valueOf(-10.23);
+
+        TransactionRequest transactionRequest = TransactionMock.createTransactionRequest(OperationType.PAYMENT, amount);
+
+        BusinessException businessException = assertThrows(BusinessException.class, () -> transactionService.execute(transactionRequest));
+
+        assertEquals(String.format("The operation [%s] cannot have negative amount [%s].", OperationType.PAYMENT, amount), businessException.getMessage());
+    }
+
+
 }
