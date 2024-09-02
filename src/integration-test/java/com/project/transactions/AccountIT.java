ee6d64a1b9d11d248e2b222bf9f113f01757b14d
Guilherme Canuto <ganuto@hotmail.com>
2024-09-02 17:48:32 -0300

    Added new AccountIT tests

--- src/integration-test/java/com/project/transactions/AccountIT.java
+++ src/integration-test/java/com/project/transactions/AccountIT.java
@@ -1,10 +1,13 @@
 package com.project.transactions;
 
+import static org.junit.Assert.assertTrue;
+
 import com.project.transactions.controller.data.request.AccountCreationRequest;
 import com.project.transactions.domain.Account;
 import com.project.transactions.mock.AccountMock;
 import com.project.transactions.repository.AccountRepository;
 import java.io.IOException;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.jupiter.api.Order;
 import org.junit.runner.RunWith;
@@ -42,5 +45,36 @@ public class AccountIT extends TransactionsApplicationIT {
         .statusCode(HttpStatus.OK.value());
 
     Account account = accountRepository.findById(accountId).get();
+  }
+
+  @Test
+  public void createAccountUnsuccessfully() throws IOException {
+    AccountCreationRequest accountCreationRequest = new AccountCreationRequest();
+    accountCreationRequest.setDocumentNumber("ABC");
+
+    IntegrationRequests.post("/accounts", accountCreationRequest)
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.BAD_REQUEST.value());
+
+    Optional<Account> accountOptional =
+        accountRepository.findByDocumentNumber(accountCreationRequest.getDocumentNumber());
+
+    assertTrue(accountOptional.isEmpty());
+  }
+
+  @Test
+  public void getAccountUnsuccessfully() {
+    Long accountId = -1L;
+
+    IntegrationRequests.get(String.format("/accounts?accountId=%s", accountId))
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.BAD_REQUEST.value());
+
+    Optional<Account> accountOptional =
+            accountRepository.findById(accountId);
+
+    assertTrue(accountOptional.isEmpty());
   }
 }
